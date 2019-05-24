453c8b27b28a57bea70cbf56893e69296d4c53ca
Mark Pearyer <marcusp619@gmail.com>
2019-05-24 09:26:29 -0600

    Initial Commit

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,27 +1,16 @@
+import React, { Component } from "react";
+
 class AddTodoForm extends Component {
   constructor(props) {
     super(props);
     this.state = { text: "" };
   }
 
-  handleChange = e => {
-    this.setState({ text: e.target.value });
-  };
-
-  submitForm = e => {
-    e.preventDefault();
-    this.setState({ text: "" });
-  };
-
   render() {
     return (
       <section>
-        <form onSubmit={this.submitForm}>
-          <input
-            value={this.state.text}
-            placeholder="Add A Todo"
-            onChange={this.handleChange}
-          />
+        <form>
+          <input placeholder="Add A Todo" />
           <button>Add Todo</button>
         </form>
       </section>
