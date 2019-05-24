659904291d855d24536320af372ccb12f5d91a07
Mark Pearyer <marcusp619@gmail.com>
2019-05-24 08:54:58 -0600

    Initial Setup

---
+++ src/components/AddTodoForm.js
@@ -0,0 +1,32 @@
+class AddTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  submitForm = e => {
+    e.preventDefault();
+    this.setState({ text: "" });
+  };
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.submitForm}>
+          <input
+            value={this.state.text}
+            placeholder="Add A Todo"
+            onChange={this.handleChange}
+          />
+          <button>Add Todo</button>
+        </form>
+      </section>
+    );
+  }
+}
+
+export default AddTodoForm;
