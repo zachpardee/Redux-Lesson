03ce242e7c0d87855723ca4b07e3d8a746ca9728
Mark Pearyer <marcusp619@gmail.com>
2019-05-24 13:09:21 -0600

    refactor class to functional component

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -3,14 +3,12 @@ import { connect } from "react-redux";
 import TodoItem from "./TodoItem";
 import { toggleTodo } from "../actions";
 
-class TodosContainer extends React.Component {
-  render() {
-    let todos = this.props.todos.map(todo => (
-      <TodoItem key={todo.id} {...todo} toggleTodo={this.props.toggleTodo} />
-    ));
-    return <div>{todos}</div>;
-  }
-}
+const TodosContainer = props => {
+  let todos = this.props.todos.map(todo => (
+    <TodoItem key={todo.id} {...todo} toggleTodo={this.props.toggleTodo} />
+  ));
+  return <div>{todos}</div>;
+};
 
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
